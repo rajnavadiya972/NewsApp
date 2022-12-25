3de3375bce3347be2127904d971739dc025d9ffa
rajnavadiya972 <rajnavadiya333@gmail.com>
2022-12-25 17:09:29 +0530

    Login

---
+++ App.js
@@ -0,0 +1,20 @@
+// import logo from './logo1.png';
+// import React, { useState } from 'react';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+// import Icons from 'react-native-vector-icons/SimpleLineIcons';
+// import { StyleSheet, Text, View, Image, TextInput, Pressable, ScrollView } from 'react-native';
+import Login from './component/Login.js';
+import Verify from './component/Verify';
+
+
+
+export default function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <Verify />
+    </>
+  );
+}
+
+
---
+++ component/Login.js
@@ -0,0 +1,138 @@
+import logo from '../images/logo1.png';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icons from 'react-native-vector-icons/SimpleLineIcons';
+import { StyleSheet, Text, View, Image, TextInput, Pressable, ScrollView } from 'react-native';
+
+
+export default function Login() {
+    const [passSet, setPassSet] = useState(false);
+    var passEye;
+    if (passSet) {
+        passEye = <Icon name="visibility" style={{ marginTop: -30, marginRight: 10, marginLeft: 'auto' }} size={25} onPress={() => setPassSet(false)} />;
+    } else {
+        passEye = <Icon name="visibility-off" style={{ marginTop: -30, marginRight: 10, marginLeft: 'auto' }} size={25} onPress={() => setPassSet(true)} />
+    }
+
+    return (
+        <ScrollView style={styles.maincontainer}>
+            <View style={styles.container}>
+                <Text style={{ marginTop: 40, fontSize: 20, fontWeight: "600" }}>World News</Text>
+                <Image source={logo}
+                    style={{ width: 250, height: 200 }}
+                />
+            </View>
+            <View>
+                <View style={styles.containerInfo}>
+                    <Text style={styles.text}>Username</Text>
+                    <TextInput
+                        style={styles.textfild}
+                        placeholder="Username"
+                        selectionColor={'black'}
+                    />
+                </View>
+                <View style={styles.containerInfo}>
+                    <Text style={styles.text}>Email</Text>
+                    <TextInput
+                        style={styles.textfild}
+                        placeholder="Email"
+                        autoCapitalize="none"
+                        selectionColor={'black'}
+                    />
+                </View>
+                <View style={styles.containerInfo}>
+                    <Text style={styles.text}>Password</Text>
+                    <View>
+                        <TextInput
+                            style={styles.textfildPass}
+                            placeholder="Password"
+                            selectionColor={'black'}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            secureTextEntry={!passSet}
+                        />
+                        {passEye}
+                    </View>
+                    <Text style={{ marginLeft: 'auto', marginRight: 10, marginTop: 10 }}>Forgot Password?</Text>
+                </View>
+            </View>
+            <Pressable
+                style={styles.submit}
+                underlayColor='#fff'
+                android_ripple={{ color: '#fff' }}>
+                <Text style={styles.submitText}>Sign Up</Text>
+            </Pressable>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 15, marginHorizontal: 10 }}>
+                <View style={{ flex: 1, height: 1, backgroundColor: 'black' }} />
+                <View>
+                    <Text style={{ width: 110, textAlign: 'center' }}>or sign in with</Text>
+                </View>
+                <View style={{ flex: 1, height: 1, backgroundColor: 'black' }} />
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 20, marginHorizontal: 10 }}>
+                <Icons name="social-google" style={{ marginLeft: 'auto', marginRight: 'auto' }} size={25} />
+                <Icons name="social-facebook" style={{ marginLeft: 'auto', marginRight: 'auto' }} size={25} />
+                <Icon name="mail-outline" style={{ marginLeft: 'auto', marginRight: 'auto' }} size={25} />
+            </View>
+            <View style={{ marginTop: 15, marginBottom: 15 }}>
+                <Text style={{ marginLeft: 'auto', marginRight: 'auto' }}>Don't have account?<Text style={{ fontWeight: '600' }}>Register</Text></Text>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    maincontainer: {
+        flex: 1,
+    },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerInfo: {
+        marginTop: 30
+    },
+    textfild: {
+        height: 40,
+        fontSize: 15,
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        borderWidth: 1,
+        marginHorizontal: 10,
+    },
+    textfildPass: {
+        height: 40,
+        minWidth: '85%',
+        fontSize: 15,
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        borderWidth: 1,
+        marginHorizontal: 10,
+    },
+    text: {
+        marginLeft: 10,
+        fontWeight:'500'
+    },
+    submit: {
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        width: '40%',
+        marginTop: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: '#68a0cf',
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: '#fff',
+    },
+    submitText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+});
