34f70e1f7dd49c91d2136a815f5599116c8de0c9
rajnavadiya972 <rajnavadiya333@gmail.com>
2022-12-26 21:18:29 +0530

    DeleteComment

--- App.js
+++ App.js
@@ -1,8 +1,3 @@
-// import logo from './logo1.png';
-// import React, { useState } from 'react';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
-// import Icons from 'react-native-vector-icons/SimpleLineIcons';
-// import { StyleSheet, Text, View, Image, TextInput, Pressable, ScrollView } from 'react-native';
 import Login from './component/Login.js';
 import Verify from './component/Verify';
 import { useColorScheme } from 'react-native';
