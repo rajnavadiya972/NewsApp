f0a858f1bd0c8c54f8b7b933b0115e72edd8e1ec
rajnavadiya972 <rajnavadiya333@gmail.com>
2022-12-30 21:33:28 +0530

    AddOnSwipe

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import Login from './component/Login.js';
 import Verify from './component/Verify';
 import Register from './component/Register';
-<<<<<<< HEAD
-=======
 import OnSwipe from './component/OnSwipe';
-import { useColorScheme } from 'react-native';
->>>>>>> 3fa8463fa4689f12951e9a5fb13183d81aa86356
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -22,11 +18,7 @@ export default function App() {
   return (
     <>
       <NavigationContainer theme={MyTheme}>
-<<<<<<< HEAD
-        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown:false}}>
-=======
         <Stack.Navigator initialRouteName="onSwipe" screenOptions={{headerShown:false}} carsS>
->>>>>>> 3fa8463fa4689f12951e9a5fb13183d81aa86356
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Verify" component={Verify} />
           <Stack.Screen name="Register" component={Register} />
