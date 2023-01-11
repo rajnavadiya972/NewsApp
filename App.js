5eb268eec016c7ff941f8b600b2dba75a7dce122
vdpanchani71 <vdpanchani05@gmail.com>
2023-01-11 19:55:45 +0530

    done

--- App.js
+++ App.js
@@ -5,10 +5,14 @@ import OnSwipe from './component/OnSwipe';
 import BottomHome from './component/BottomHome';
 import SettingIn from './SettingsTab/SettingsIn.js';
 import Setting from './BottomTab/Setting.js';
+import BoostPost from './SettingsComponent/BoostPost';
+import AddDoc from './SettingsComponent/AddDoc';
+import DocAdd from './SettingsComponent/DocAdd.js';
 import { useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GoogleLogin from './component/GoogleSignUp.js';
+import { Text } from 'react-native';
 
 const MyTheme = {
   dark: false,
@@ -22,8 +26,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <NavigationContainer theme={MyTheme}>
-        <Stack.Navigator initialRouteName="BottomHome" screenOptions={{headerShown:false}}>
+      {/* <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator initialRouteName="AddImg" screenOptions={{headerShown:false}}>
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Verify" component={Verify} />
           <Stack.Screen name="Register" component={Register} />
@@ -31,9 +35,15 @@ export default function App() {
           <Stack.Screen name="BottomHome" component={BottomHome} />
           <Stack.Screen name="SettingIn" component={SettingIn} />
           <Stack.Screen name="Setting" component={Setting} />
+          <Stack.Screen name="BoostPost" component={BoostPost} />
+          <Stack.Screen name="AddImg" component={AddImg} />
         </Stack.Navigator>
-      </NavigationContainer>
+      </NavigationContainer> */}
       {/* <GoogleLogin /> */}
+      {/* <BoostPost /> */}
+      {/* <BoostPost /> */}
+      {/* <Text>hggs</Text> */}
+      <DocAdd/>
     </>
   );
 }
